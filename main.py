36619975788739db07f8246885b300c77ecb5dc0
gayashan4lk <gayashan4lk@gmail.com>
2025-04-05 10:50:10 +0530

    create hero update endpoint

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import os
+from contextlib import asynccontextmanager
+
 from dotenv import load_dotenv
 from typing import Annotated
 from fastapi import FastAPI, Depends, Query, HTTPException
@@ -6,11 +8,24 @@ from sqlmodel import Field, Session, SQLModel, create_engine, select
 
 load_dotenv()
 
-class Hero(SQLModel, table=True):
-    id: int = Field(default=None, primary_key=True)
+class HeroBase(SQLModel):
     name: str = Field(index=True)
     age: int | None = Field(default=None, index=True)
+
+class Hero(HeroBase, table=True):
+    id: int = Field(default=None, primary_key=True)
     secret_name: str
+
+class HeroPublic(HeroBase):
+    id:int
+
+class HeroCreate(HeroBase):
+    secret_name: str
+
+class HeroUpdate(HeroBase):
+    name: str | None = None
+    age: int | None = None
+    secret_name: str | None = None
 
 database_url = os.getenv("DATABASE_URL")
 print(f"database_url: {database_url}")
@@ -26,21 +41,27 @@ def get_session():
 
 SessionDep = Annotated[Session, Depends(get_session)]
 
-app = FastAPI()
-
-@app.on_event("startup")
-def on_startup():
+@asynccontextmanager
+async def lifespan(app: FastAPI):
     create_db_and_tables()
+    yield
 
-@app.post("/heroes")
-def create_hero(hero: Hero, session: SessionDep):
-    session.add(hero)
+app = FastAPI(lifespan=lifespan)
+
+@app.post("/heroes", response_model=HeroPublic)
+def create_hero(hero: HeroCreate, session: SessionDep):
+    db_hero = Hero.model_validate(hero)
+    session.add(db_hero)
     session.commit()
-    session.refresh(hero)
-    return hero
+    session.refresh(db_hero)
+    return db_hero
 
-@app.get("/heroes")
-def read_heroes(session: SessionDep, offset: int = 0, limit: Annotated[int, Query(le=100)] = 100) -> list[Hero]:
+@app.get("/heroes", response_model=list[HeroPublic])
+def read_heroes(
+        session: SessionDep,
+        offset: int = 0,
+        limit: Annotated[int, Query(le=100)] = 100
+):
     heroes = session.exec(select(Hero).offset(offset).limit(limit)).all()
     return heroes
 
@@ -50,6 +71,18 @@ def read_hero(hero_id: int, session: SessionDep):
     if not hero:
         raise HTTPException(status_code=404, detail="Hero not found")
     return hero
+
+@app.patch("/heroes/{hero_id}", response_model=HeroPublic)
+def update_hero(hero_id: int, hero: HeroUpdate, session: SessionDep):
+    hero_db = session.get(Hero, hero_id)
+    if not hero_db:
+        raise HTTPException(status_code=404, detail="Hero not found")
+    hero_data = hero.model_dump(exclude_unset=True)
+    hero_db.sqlmodel_update(hero_data)
+    session.add(hero_db)
+    session.commit()
+    session.refresh(hero_db)
+    return hero_db
 
 @app.delete("/heroes/{hero_id}")
 def delete_hero(hero_id:int, session: SessionDep):
